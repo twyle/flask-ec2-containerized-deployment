cf106b6d419c14f1b141765af282ec1bbfb0ba7a
lyleokoth <okothlyleochieng@gmail.com>
2022-06-26 20:00:24 +0300

    feat: added logging.

--- api/blueprints/api/helpers.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-"""This module has methods that are used in the other modules in this package."""
--- api/blueprints/api/models.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-"""This module contains the database models used by the auth blueprint."""
-from dataclasses import dataclass
